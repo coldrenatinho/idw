96eceae67ea5c3b1a88f295b43c86b7c1287cdca
Renato A. Silva <97402553+coldrenatinho@users.noreply.github.com>
2024-11-18 23:37:18 -0400

    Adicionado image de fundo ao contato

--- aula_01/locadoraPatinhas/js/main.js
+++ aula_01/locadoraPatinhas/js/main.js
@@ -1,3 +1,25 @@
+document.addEventListener('DOMContentLoaded', function() {
+  var dropdownToggles = document.querySelectorAll('.dropdown-toggle');
+
+  dropdownToggles.forEach(function(toggle) {
+    toggle.addEventListener('click', function(event) {
+      event.preventDefault();
+      var dropdownMenu = this.nextElementSibling;
+      dropdownMenu.style.display = dropdownMenu.style.display === 'block' ? 'none' : 'block';
+    });
+  });
+
+  // Fechar o dropdown se clicar fora dele
+  window.addEventListener('click', function(event) {
+    if (!event.target.matches('.dropdown-toggle')) {
+      var dropdowns = document.querySelectorAll('.dropdown-menu');
+      dropdowns.forEach(function(dropdown) {
+        dropdown.style.display = 'none';
+      });
+    }
+  });
+});
+
 function newWindow(url) {
     window.open(url, '_blank'); 
     console.log('Nova janela aberta');
